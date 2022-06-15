85b22e24a84716c67f23322c3f783990ffea1c40
Vezil <SzymonWojaczek@wp.pl>
2022-06-15 23:19:53 +0200

    remove then (test)

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,4 @@ const server = new ApolloServer({
     context: createContext
 });
 
-server.listen({ port: process.env.PORT || 4000, }).then(({ url }) => {
-    console.log(`Server ready at: ${url})`);
-});
+server.listen({ port: process.env.PORT || 4000 });
