b14817cbc26e2eba7ba37abc4f05775248667fa1
Vezil <SzymonWojaczek@wp.pl>
2022-06-15 22:20:22 +0200

    console log test data

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,9 @@ import { schema } from './schema';
 const server = new ApolloServer({
     schema,
     context: createContext,
-    introspection: true,
 });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`Server ready at: ${url})`);
-});
+server.listen({ port: process.env.PORT || 4000 }).then((data) => {
+    console.log(`data test: ${data})`);
+})
 
