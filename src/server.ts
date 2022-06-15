72aa7ade6cdf142475af308d2c682628ea3e7fc0
Vezil <SzymonWojaczek@wp.pl>
2022-06-15 20:40:36 +0200

    plugin production test

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import { ApolloServer } from 'apollo-server';
+import {
+    ApolloServerPluginLandingPageProductionDefault,
+    ApolloServerPluginLandingPageLocalDefault,
+} from 'apollo-server-core';
 import { createContext } from './context';
 import { schema } from './schema';
 
+
 const server = new ApolloServer({
     schema,
-    context: createContext
+    context: createContext,
+    plugins: [
+        process.env.NODE_ENV === 'production' ?
+            ApolloServerPluginLandingPageProductionDefault({ footer: false }) :
+            ApolloServerPluginLandingPageLocalDefault({ footer: false })
+    ]
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
