1ce70c08706ae511b2c35cf55f0627ffbe96096c
Vezil <SzymonWojaczek@wp.pl>
2022-06-15 21:09:35 +0200

    restore changes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,10 @@
 import { ApolloServer } from 'apollo-server';
-import {
-    ApolloServerPluginLandingPageProductionDefault,
-    ApolloServerPluginLandingPageLocalDefault,
-} from 'apollo-server-core';
 import { createContext } from './context';
 import { schema } from './schema';
 
-
 const server = new ApolloServer({
     schema,
-    context: createContext,
-    plugins: [
-        process.env.NODE_ENV === 'production' ?
-            ApolloServerPluginLandingPageProductionDefault({ footer: false }) :
-            ApolloServerPluginLandingPageLocalDefault({ footer: false })
-    ]
+    context: createContext
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
